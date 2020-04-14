1945d1fcc21ee37b6a8399610b59c4983c5c9223
Haythem Mabrouk <haythem.mabrouk@gmail.com>
2020-04-14 15:48:20 +0200

    Update InitProjectCommand.php

--- src/Command/InitProjectCommand.php
+++ src/Command/InitProjectCommand.php
@@ -24,7 +24,7 @@ class InitProjectCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
         $io = new SymfonyStyle($input, $output);
-        $io->writeln("Initilizing project on process");
+        $io->writeln("Initiating project on process");
 
         $command = $this->getApplication()->find('doctrine:database:drop');
         $commandInput  = new ArrayInput(["--force" => true, '--if-exists' => true]);
@@ -44,7 +44,7 @@ class InitProjectCommand extends Command
         $commandInput->setInteractive(false);
         $returnCode = $command->run($commandInput, $output);
 
-        $io->success('Project successely initilized');
+        $io->success('the project is successfully initiated');
 
     }
 }
